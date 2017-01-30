3b58c98e9a0146dc2e00c942a6f1af386ddf3a03
Kayla Brewer <cooper.kaylam@gmail.com>
2017-01-30 13:24:52 -0600

    Completed sammich

--- javascripts/SandwichMaker.js
+++ javascripts/SandwichMaker.js
@@ -1,3 +1,5 @@
+"use strict"
+
 var SandwichMaker = (function() {
 
   // Private variable to store the price
@@ -7,6 +9,8 @@ var SandwichMaker = (function() {
   return {
     addTopping: function(toppingPrice) {
       totalPrice += toppingPrice;
+      console.log("The total price is now: " + totalPrice);
+      document.getElementById("finalSandwich").innerHTML = `<h2>The total price is now:` + totalPrice; '</h2>';
     }
   };
 })();
